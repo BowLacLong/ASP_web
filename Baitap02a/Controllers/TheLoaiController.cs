c093b40ac9cd040dc2f9cac16b85d79de31569e0
BowLacLong <hailuanvu1234@gmail.com>
2024-08-28 14:57:21 +0700

    Baitap02a

---
+++ Baitap02a/Controllers/TheLoaiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Baitap02a.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Baitap02a/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Baitap02a.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
