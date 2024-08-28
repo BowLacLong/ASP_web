c19cbc64fd43dd7fd1d813848308896711830ae6
BowLacLong <hailuanvu1234@gmail.com>
2024-08-28 13:51:49 +0700

    Em da hoan thanh Baitap02

--- Baitap02/obj/Debug/net8.0/Baitap02.AssemblyInfo.cs
+++ Baitap02/obj/Debug/net8.0/Baitap02.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Baitap02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3e7de54f3244784963106d8afc841fe4add77e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6415e4550b727fc428b7aba65cde178c17eb84d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Baitap02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Baitap02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
