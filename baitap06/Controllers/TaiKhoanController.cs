eb678a0f7dfea71e99f658c554f7d5ab1d03c06a
BowLacLong <hailuanvu1234@gmail.com>
2024-09-11 15:59:14 +0700

    baitap06

---
+++ baitap06/Controllers/TaiKhoanController.cs
@@ -0,0 +1,22 @@
+﻿using baitap06.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace baitap06.Controllers
+{
+    public class TaiKhoanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DangKy( taikhoanViewModel taikhoan)
+        {
+            if (taikhoan != null && taikhoan.Password != null && taikhoan.Password.Length > 0)
+            {
+                taikhoan.Password = taikhoan.Password + "_đã_mã_hóa";
+            }
+            return View(taikhoan);
+        }
+    }
+}
---
+++ baitap06/Models/taikhoanViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace baitap06.Models
+{
+    public class taikhoanViewModel
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
