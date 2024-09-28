fa52b24b162719c1bcce091989780c8b1418d8f1
BowLacLong <hailuanvu1234@gmail.com>
2024-09-28 15:29:12 +0700

    baikiemtra02

---
+++ baikiemtra02/baikiemtra02/Models/LopHoc.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace baikiemtra02.Models
+{
+    public class LopHoc
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string? TenLopHoc { get; set; }
+        [Required]
+        public int SoLuongSinhVien { get; set; }
+        [Display(Name = "Năm Nhập Học")]
+        public DateTime NamNhapHoc { get; set; }
+        [Display(Name = "Năm Ra Trường")]
+        public DateTime NamRaTruong { get; set; }
+    }
+}
